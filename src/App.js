9dd9ff28e73973ab12825d63228c73075c66910f
Aniruddh Parwal <aniruddhparwal@gmail.com>
2021-05-04 20:58:08 +0530

    Location fetch api updated

--- src/App.js
+++ src/App.js
@@ -13,24 +13,70 @@ function App() {
       //   lat: position.coords.latitude,
       //   lon: position.coords.longitude,
       // });
-      fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.aeebe5b1021eaf94b37fbeddaef781f0&lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`)
+      //   fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.aeebe5b1021eaf94b37fbeddaef781f0&lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`)
+      //     .then(response => response.json())
+      //     .then(Locationdata => {
+      //       console.log("location data", Locationdata)
+      //       console.log("aa", Locationdata.address.postcode)
+      //       fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/states`)
+      //         .then(responsestate => responsestate.json())
+      //         .then(StateList => {
+      //           console.log(StateList)
+      //           StateList.states.map(state => {
+      //             if (state.state_name === Locationdata.address.state) {
+      //               console.log("state_id is ", state.state_id)
+      //               fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state.state_id}`)
+      //                 .then(responsedistrict => responsedistrict.json())
+      //                 .then(DistrictData => {
+      //                   console.log(DistrictData)
+      //                   DistrictData.districts.map(district => {
+      //                     if (district.district_name === Locationdata.address.city) {
+      //                       console.log("district_id is ", district.district_id)
+      //                       fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district.district_id}&date=04-05-2021`)
+      //                         .then(response => response.json())
+      //                         .then(data => {
+      //                           setData(data)
+      //                           setDataAvailable(true)
+      //                           console.log("Main data", data)
+      //                         })
+      //                     }
+      //                   })
+
+      //                 })
+      //             }
+      //           })
+      //         })
+
+      //       // fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${data.address.postcode}&date=04-05-2021`)
+      //       //   .then(response => response.json())
+      //       //   .then(data => {
+      //       //     setData(data)
+      //       //     setDataAvailable(true)
+      //       //     console.log(data)
+      //       //   })
+      //     })
+
+      // })
+
+
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&language=en&key=AIzaSyAlELmqkRobpn26ReMLLTirp7GHsaW8vy0`)
         .then(response => response.json())
         .then(Locationdata => {
           console.log("location data", Locationdata)
-          console.log("aa", Locationdata.address.postcode)
+          // console.log("aa", Locationdata.address.postcode)
           fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/states`)
             .then(responsestate => responsestate.json())
             .then(StateList => {
               console.log(StateList)
               StateList.states.map(state => {
-                if (state.state_name === Locationdata.address.state) {
+                if (state.state_name === Locationdata.results[5].address_components[1].long_name) {
                   console.log("state_id is ", state.state_id)
                   fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state.state_id}`)
                     .then(responsedistrict => responsedistrict.json())
                     .then(DistrictData => {
                       console.log(DistrictData)
                       DistrictData.districts.map(district => {
-                        if (district.district_name === Locationdata.address.city) {
+                        if (district.district_name === Locationdata.results[5].address_components[0].long_name) {
                           console.log("district_id is ", district.district_id)
                           fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district.district_id}&date=04-05-2021`)
                             .then(response => response.json())
@@ -57,6 +103,10 @@ function App() {
         })
 
     })
+
+
+
+
     console.log("app")
     // fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=461331&date=04-05-2021')
     //   .then(response => response.json())
